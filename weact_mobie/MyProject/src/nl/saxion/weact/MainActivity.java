a50bab06e88eae52c12178f7d326d75cfc5264c7
mikeholtkamp <327450@student.saxion.nl>
2013-12-05 09:10:15 +0100

    new fragment

--- weact_mobie/MyProject/src/nl/saxion/weact/MainActivity.java
+++ weact_mobie/MyProject/src/nl/saxion/weact/MainActivity.java
@@ -129,18 +129,42 @@ public class MainActivity extends Activity {
 
     private void selectItem(int position) {
         // update the main content by replacing fragments
-        Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
+    	
+    	
+    	String action = mPlanetTitles[position];
+    	Fragment fragment;
+    	if(action.equals("Profiel aanpassen"))
+    	{
+    		fragment = new ProfileAanpassen();
+    	}
+    	else
+    	{
+    		fragment = new PlanetFragment();
+	        Bundle args = new Bundle();
+	        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+	        fragment.setArguments(args);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+	        
+//	        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+//
+//	        // update selected item and title, then close the drawer
+//	        mDrawerList.setItemChecked(position, true);
+//	        setTitle(mPlanetTitles[position]);
+//	        mDrawerLayout.closeDrawer(mDrawerList);
+    	}
+    	FragmentManager fragmentManager = getFragmentManager();
+    	fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
         // update selected item and title, then close the drawer
         mDrawerList.setItemChecked(position, true);
         setTitle(mPlanetTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
+    	
+   
+			
+	       
+		
+        
     }
 
     @Override
